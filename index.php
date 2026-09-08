fed44ca0a243392b1a9e0db236dbf72de209b72f
sourabh sharma <sharmasourabh25006@gmail.com>
2026-09-08 21:18:15 +0530

    initial project

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Online Quiz Management System</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <h1>Online Quiz Management System</h1>
+
+        <p>Test your knowledge with our online quizzes!</p>
+
+        <div class="buttons">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+
+    </div>
+
+</body>
+</html>
